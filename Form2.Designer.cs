a01aad2138ff1b651b6e0b847f5cef673a29c406
Recep Çelimli <98876147+celimlirecep@users.noreply.github.com>
2022-02-14 21:29:18 +0300

    cifmi_tekmi_form

---
+++ Form2.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace Form_01_temel_kontroller
+{
+    partial class btnSay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lstKisiler = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtaranan = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnara = new System.Windows.Forms.Button();
+            this.lblbulunan = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Tan;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(33, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD SOYAD :";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.BackColor = System.Drawing.Color.Turquoise;
+            this.txtAdSoyad.Location = new System.Drawing.Point(172, 43);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(90, 20);
+            this.txtAdSoyad.TabIndex = 0;
+            this.txtAdSoyad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAdSoyad_KeyPress);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.Blue;
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(38, 75);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(95, 56);
+            this.btnEkle.TabIndex = 3;
+            this.btnEkle.TabStop = false;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lstKisiler
+            // 
+            this.lstKisiler.BackColor = System.Drawing.Color.Yellow;
+            this.lstKisiler.FormattingEnabled = true;
+            this.lstKisiler.Location = new System.Drawing.Point(38, 149);
+            this.lstKisiler.Name = "lstKisiler";
+            this.lstKisiler.Size = new System.Drawing.Size(222, 173);
+            this.lstKisiler.TabIndex = 2;
+            this.lstKisiler.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(130, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "KİSİ SAYISI";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtaranan
+            // 
+            this.txtaranan.Location = new System.Drawing.Point(332, 47);
+            this.txtaranan.Name = "txtaranan";
+            this.txtaranan.Size = new System.Drawing.Size(100, 20);
+            this.txtaranan.TabIndex = 1;
+            this.txtaranan.TextChanged += new System.EventHandler(this.txtaranan_TextChanged);
+            this.txtaranan.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtaranan_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Tan;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(349, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "arama";
+            // 
+            // btnara
+            // 
+            this.btnara.BackColor = System.Drawing.Color.Blue;
+            this.btnara.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnara.Location = new System.Drawing.Point(332, 75);
+            this.btnara.Name = "btnara";
+            this.btnara.Size = new System.Drawing.Size(126, 56);
+            this.btnara.TabIndex = 4;
+            this.btnara.TabStop = false;
+            this.btnara.Text = "ara";
+            this.btnara.UseVisualStyleBackColor = false;
+            this.btnara.Click += new System.EventHandler(this.btnara_Click);
+            // 
+            // lblbulunan
+            // 
+            this.lblbulunan.AutoSize = true;
+            this.lblbulunan.BackColor = System.Drawing.Color.Tan;
+            this.lblbulunan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblbulunan.Location = new System.Drawing.Point(339, 149);
+            this.lblbulunan.Name = "lblbulunan";
+            this.lblbulunan.Size = new System.Drawing.Size(0, 25);
+            this.lblbulunan.TabIndex = 0;
+            // 
+            // btnSay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(492, 335);
+            this.Controls.Add(this.txtaranan);
+            this.Controls.Add(this.lstKisiler);
+            this.Controls.Add(this.btnara);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.lblbulunan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "btnSay";
+            this.Text = "Form2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.btnSay_FormClosing);
+            this.Load += new System.EventHandler(this.btnSay_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lstKisiler;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtaranan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnara;
+        private System.Windows.Forms.Label lblbulunan;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_01_temel_kontroller
+{
+    public partial class btnSay : Form
+    {
+        public btnSay()
+        {
+            InitializeComponent();
+        }
+
+        void ekle()
+        {
+            lstKisiler.Items.Add(txtAdSoyad.Text.ToUpper().Trim());
+            // txtAdSoyad.Clear();
+            //txtAdSoyad.Text = "";
+            txtAdSoyad.ResetText();
+            txtAdSoyad.Focus();
+
+        }
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            ekle();
+        }
+
+        private void txtAdSoyad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            //e.Handled = e.KeyChar == (char)Keys.Space;
+            if (e.KeyChar==(char)Keys.Enter)
+            {
+                ekle();
+                string adSoyad = txtAdSoyad.Text;
+                int uzunluk = adSoyad.Length;
+
+                if (uzunluk > 0)
+                {
+
+                    int konum = adSoyad.Length - 1;//karakter sayısı -1 dizi sırası
+
+                    if (adSoyad[konum].ToString() == " ")
+                    {
+                        e.Handled = e.KeyChar == (char)Keys.Space;
+                    }
+                }
+                else
+                {
+                    e.Handled = e.KeyChar == (char)Keys.Space;
+                }
+            }
+
+            
+          
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"KİŞİ SAYISI: {lstKisiler.Items.Count}");
+        }
+        void arama()
+        {
+            string aranan = txtaranan.Text.ToUpper();
+            if (lstKisiler.Items.Contains(aranan))
+            {
+                lblbulunan.Text = aranan;
+            }
+            else
+            {
+                lblbulunan.Text = "aradığınız kayıt bulunamadı";
+            }
+            txtaranan.Clear();
+        }
+
+        private void btnara_Click(object sender, EventArgs e)
+        {
+            arama();
+        }
+        
+
+        private void txtaranan_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if ((char)Keys.Enter == e.KeyChar)
+            {
+                arama();
+            }
+            
+        }
+
+        private void txtaranan_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSay_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSay_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+
+      
+    }
+}
